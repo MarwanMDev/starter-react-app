8358d9eb60f40e959c1693d74c716908daafb5ae
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-29 17:56:52 +0200

    Portfolio Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Experience />
       <Services />
+      <Portfolio />
     </>
   );
 };
