29b79d9d92f98ba31da77d1d247f7e6dad9450b0
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-29 13:13:58 +0200

    page header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,28 @@
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import HeaderImage from '../../assets/home-header-image.jpg';
+import './header.css';
+
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Marwan Mostafa</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          {/* <img src={HeaderImage} alt="me"></img> */}
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { FiDribbble } from 'react-icons/fi';
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/marwan-mostafa/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/MarwanMDev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </a>
+      <a
+        href="https://dribbble.com/MarwanMDev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FiDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
